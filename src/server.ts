974fbacc3de3e24886721f8c7bf63e2bbad5fbda
Edio Gama <ediogama@hotmail.com>
2021-03-21 13:52:25 -0300

    add ts init

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
-import express from "express";
+import express, { request } from "express";
 
 const app = express();
+
+app.get('/', (request, response) => {
+    return response.json({message: 'Hello world!'});
+});
 
 app.listen(3333, () => console.log('Server is running!'));
