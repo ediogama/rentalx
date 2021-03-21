d9a68f5808a0afbeb1091756fbafbbe94a26a855
Edio Gama <ediogama@hotmail.com>
2021-03-21 14:26:36 -0300

    configs all ready

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,6 @@ import express, { request } from "express";
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({message: 'Hello world!'});
-});
+app.use(express.json());
 
 app.listen(3333, () => console.log('Server is running!'));
