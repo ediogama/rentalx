e6a191794a27c30e789884af9faadd76f1267a0d
Edio Gama <ediogama@hotmail.com>
2021-03-22 19:05:23 -0300

    add changes and create useCases

---
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.ts
@@ -0,0 +1,14 @@
+import {Request, Response} from "express";
+import { CategoriesRepository } from "../../repositories/CategoriesRepository";
+
+class ListCategoriesController {
+    constructor(private categoriesRepository: CategoriesRepository) {}
+
+    handle(request: Request, response: Response): Response {
+        const categories = this.categoriesRepository.list()
+
+        return response.json(categories);
+    }
+}
+
+export { ListCategoriesController };
