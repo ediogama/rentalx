0de19126e19a7b176edfb421a99590eddd1585c7
Edio Gama <ediogama@hotmail.com>
2021-03-24 23:31:02 -0300

    add changes

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,13 +1,11 @@
 import { Router } from "express";
-import { SpecificationsRepository } from "../modules/cars/repositories/implementations/SpecificationsRepository";
+
 import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
 
 const specificationsRoutes = Router();
 
-const specificationRepository = new SpecificationsRepository();
-
 specificationsRoutes.post("/", (request, response) => {
-    return createSpecificationController.handle(request, response);
+  return createSpecificationController.handle(request, response);
 });
 
-export { specificationsRoutes }
+export { specificationsRoutes };
